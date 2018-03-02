29f504adfd726d9c21930dd8db4c226f766c748d
SDS 800 <jeremyhyman@gmail.com>
2018-03-02 16:14:43 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,8 +3,8 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
 
-  #elsif board[index] == ""
-  #  false
+  elsif board[index] == "X" || board[index] == "O" 
+    false
 
   end
 
